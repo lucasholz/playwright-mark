7c368005c1afdb6da4f254ae93fd62e6f67aaa08
Lucas Holz <matteholz@gmail.com>
2024-08-29 10:36:43 -0300

    test: create a new test and add describes

--- tests/support/pages/tasks/index.ts
+++ tests/support/pages/tasks/index.ts
@@ -20,6 +20,11 @@ export class TasksPage {
     await this.page.click('css=button >> text=Create')
   }
 
+  async toggle(taskName: string) {
+    const target = this.page.locator(`xpath=//p[text()="${taskName}"]/..//button[contains(@class, "Toggle")]`)
+    await target.click()
+  }
+
   async shouldHaveText(taskName: string) {
     const target = this.page.locator(`css=.task-item p >> text=${taskName}`)
     await expect(target).toBeVisible()
@@ -28,5 +33,10 @@ export class TasksPage {
   async alertHaveText(text: string) {
     const target = this.page.locator('.swal2-html-container')
     await expect(target).toHaveText(text)
+  }
+
+  async shouldBeDone(taskName) {
+    const target = this.page.getByText(taskName)
+    await expect(target).toHaveCSS('text-decoration-line', 'line-through')
   }
 }
--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -4,48 +4,68 @@ import { deleteTaskByHelper, postTask } from './support/helpers';
 import { TasksPage } from './support/pages/tasks';
 import data from './fixtures/tasks.json'
 
+test.describe('required fields', () => {
 
-test('must be able to register a new task', async ({ page, request }) => {
+  test('must be able to register a new task', async ({ page, request }) => {
 
-  const task = data.success as TaskModel
+    const task = data.success as TaskModel
 
-  await deleteTaskByHelper(request, task.name)
+    await deleteTaskByHelper(request, task.name)
 
-  const tasksPage: TasksPage = new TasksPage(page)
+    const tasksPage: TasksPage = new TasksPage(page)
 
-  await tasksPage.go()
-  await tasksPage.create(task)
-  await tasksPage.shouldHaveText(task.name)
+    await tasksPage.go()
+    await tasksPage.create(task)
+    await tasksPage.shouldHaveText(task.name)
 
 
-  await page.waitForTimeout(1)
-});
+    await page.waitForTimeout(1)
+  });
 
-test('should not allow duplicate task', async ({ page, request }) => {
-  const task = data.duplicate as TaskModel
+  test('should not allow duplicate task', async ({ page, request }) => {
+    const task = data.duplicate as TaskModel
 
-  await deleteTaskByHelper(request, task.name)
-  await postTask(request, task)
+    await deleteTaskByHelper(request, task.name)
+    await postTask(request, task)
 
-  const tasksPage: TasksPage = new TasksPage(page)
+    const tasksPage: TasksPage = new TasksPage(page)
 
-  await tasksPage.go()
-  await tasksPage.create(task)
-  await tasksPage.alertHaveText('Task already exists!')
+    await tasksPage.go()
+    await tasksPage.create(task)
+    await tasksPage.alertHaveText('Task already exists!')
 
-  await page.waitForTimeout(1)
-});
+    await page.waitForTimeout(1)
+  });
 
-test('required field', async ({ page }) => {
-  const task = data.required as TaskModel
+  test('required field', async ({ page }) => {
+    const task = data.required as TaskModel
 
-  const tasksPage: TasksPage = new TasksPage(page)
+    const tasksPage: TasksPage = new TasksPage(page)
 
-  await tasksPage.go()
-  await tasksPage.create(task)
+    await tasksPage.go()
+    await tasksPage.create(task)
 
-  const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
-  expect(validationMessage).toEqual('This is a required field')
+    const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
+    expect(validationMessage).toEqual('This is a required field')
 
-  await page.waitForTimeout(1)
+    await page.waitForTimeout(1)
+  })
 })
+
+test.describe('update', () => {
+
+  test('must complete a new task', async ({ page, request }) => {
+    const task = data.update as TaskModel
+
+    await deleteTaskByHelper(request, task.name)
+    await postTask(request, task)
+
+    const tasksPage: TasksPage = new TasksPage(page)
+
+    await tasksPage.go()
+    await tasksPage.toggle(task.name)
+    await tasksPage.shouldBeDone(task.name)
+
+    await page.waitForTimeout(1)
+  })
+});
